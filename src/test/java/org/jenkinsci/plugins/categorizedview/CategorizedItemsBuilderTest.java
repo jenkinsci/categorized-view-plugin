d85edc6d0e3414539f60c34733c4e4b1c1a3657e
takeuchi <g.takeuchi@gmail.com>
2014-03-20 07:07:15 -0300

    Test refactoring using mockito

--- src/test/java/org/jenkinsci/plugins/categorizedview/CategorizedItemsBuilderTest.java
+++ src/test/java/org/jenkinsci/plugins/categorizedview/CategorizedItemsBuilderTest.java
@@ -1,6 +1,8 @@
 package org.jenkinsci.plugins.categorizedview;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import hudson.model.TopLevelItem;
 
 import java.util.ArrayList;
@@ -8,7 +10,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
-import org.jenkinsci.plugins.categorizedview.mocks.TopLevelItemMock;
 import org.junit.Test;
 
 public class CategorizedItemsBuilderTest {
@@ -17,10 +18,10 @@ public class CategorizedItemsBuilderTest {
 
 	public CategorizedItemsBuilderTest() {
 		itemsToCategorize = new ArrayList<TopLevelItem>();
-		itemsToCategorize.add(new TopLevelItemMock("xa"));
-		itemsToCategorize.add(new TopLevelItemMock("ba"));
-		itemsToCategorize.add(new TopLevelItemMock("me"));
-		itemsToCategorize.add(new TopLevelItemMock("ma"));
+		itemsToCategorize.add(makeMockedItem("xa"));
+		itemsToCategorize.add(makeMockedItem("ba"));
+		itemsToCategorize.add(makeMockedItem("me"));
+		itemsToCategorize.add(makeMockedItem("ma"));
 	}
 	
 	@Test
@@ -55,11 +56,11 @@ public class CategorizedItemsBuilderTest {
 	
 	@Test
 	public void getItems_withRegex_ShouldGroupByRegex() {
-		itemsToCategorize.add(new TopLevelItemMock("8.03-bar"));
-		itemsToCategorize.add(new TopLevelItemMock("8.02-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("8.02-baz"));
-		itemsToCategorize.add(new TopLevelItemMock("8.03-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("a8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("8.03-bar"));
+		itemsToCategorize.add(makeMockedItem("8.02-foo"));
+		itemsToCategorize.add(makeMockedItem("8.02-baz"));
+		itemsToCategorize.add(makeMockedItem("8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("a8.03-foo"));
 		
 		String groupRegex = "(8...)";
 		final CategorizedItemsBuilder subject = new CategorizedItemsBuilder(itemsToCategorize, Arrays.asList(new GroupingRule(groupRegex, "")));
@@ -83,11 +84,11 @@ public class CategorizedItemsBuilderTest {
 	
 	@Test
 	public void getItems_withRegex_andNaming_ShouldGroupByRegexAndNameWithNamingRule() {
-		itemsToCategorize.add(new TopLevelItemMock("8.03-bar"));
-		itemsToCategorize.add(new TopLevelItemMock("8.02-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("8.02-baz"));
-		itemsToCategorize.add(new TopLevelItemMock("8.03-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("a8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("8.03-bar"));
+		itemsToCategorize.add(makeMockedItem("8.02-foo"));
+		itemsToCategorize.add(makeMockedItem("8.02-baz"));
+		itemsToCategorize.add(makeMockedItem("8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("a8.03-foo"));
 		
 		String groupRegex = "(8...)";
 		final CategorizedItemsBuilder subject = new CategorizedItemsBuilder(itemsToCategorize, Arrays.asList(new GroupingRule(groupRegex, "foo $1")));
@@ -110,12 +111,12 @@ public class CategorizedItemsBuilderTest {
 	
 	@Test
 	public void buildRegroupedItems_withListOfRegexAndNamingRules_ShouldUseAllRulesToCategorize() {
-		itemsToCategorize.add(new TopLevelItemMock("8.03-bar"));
-		itemsToCategorize.add(new TopLevelItemMock("8.02-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("8.02-baz"));
-		itemsToCategorize.add(new TopLevelItemMock("8.03-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("a8.03-foo"));
-		itemsToCategorize.add(new TopLevelItemMock("m8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("8.03-bar"));
+		itemsToCategorize.add(makeMockedItem("8.02-foo"));
+		itemsToCategorize.add(makeMockedItem("8.02-baz"));
+		itemsToCategorize.add(makeMockedItem("8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("a8.03-foo"));
+		itemsToCategorize.add(makeMockedItem("m8.03-foo"));
 		
 		List<GroupingRule> rules = Arrays.asList(
 				new GroupingRule("(8...)", "Foo $1"),
@@ -153,5 +154,11 @@ public class CategorizedItemsBuilderTest {
 			sb.append("\n");
 		}
 		return sb.toString();
+	}
+	
+	private TopLevelItem makeMockedItem(String value) {
+		TopLevelItem mockedItem = mock(TopLevelItem.class);
+		when(mockedItem.getName()).thenReturn(value);
+		return mockedItem;
 	}
 }
