28168821fcd673428728db27515654e9b8a600c5
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-11-21 12:41:49 -0500

    Remove inline JS script and onClick handler

--- src/main/resources/lib/categorizedview/categorized-view-row.js
+++ src/main/resources/lib/categorizedview/categorized-view-row.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll(".restoreJobGroupCollapseStateParams").forEach(function(elem) {
-        const view = elem.dataset.view;
-        const groupClass = elem.dataset.jobgroup;
+
+    const rows = document.querySelectorAll("[categoryRole='category']");
+
+    rows.forEach(function(row) {
+        const holder = row.querySelector(".view-group-data-holder");
+
+        const view = holder.dataset.view;
+        const groupClass = holder.dataset.jobgroup;
+
+        row.addEventListener("click", function(e) {
+            toggleJobGroupVisibility(view, groupClass);
+        });
+
         restoreJobGroupCollapseState(view, groupClass);
     });
 });
