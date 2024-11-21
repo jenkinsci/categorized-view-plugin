202e42e2ec2e8166641af5ea82fa84fa5c6214e0
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-11-21 11:38:15 -0500

    Remove inline JS script

--- src/main/resources/lib/categorizedview/categorized-view.js
+++ src/main/resources/lib/categorizedview/categorized-view.js
@@ -1,5 +1,4 @@
 window.addEventListener("DOMContentLoaded", function() {
-    console.log("DOMContentLoaded");
     document.querySelectorAll("table.categorizedSortable").forEach(function (e){
         e.sortable = new CategorizedSortable.CategorizedSortable(e);
     })
